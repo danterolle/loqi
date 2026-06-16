dea761bfb1de1ffcf1904b8163859bbdd7e3e311
Dario Camonita <danterolle@parrotsec.org>
2026-06-17 01:58:18 +0200

    refactor: split TUI update and view logic into separate files

--- tui/update.go
+++ tui/update.go
@@ -2,7 +2,6 @@ package tui
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -163,57 +162,4 @@ func (m Model) retreatFocus() Model {
 		m.focused = focusTgtLang
 	}
 	return m
-}
-
-func (m Model) View() string {
-	if !m.ready {
-		return "\n  Loading..."
-	}
-
-	var b strings.Builder
-
-	srcName := m.langNames[m.langCodes[m.srcIdx]]
-	tgtName := m.langNames[m.langCodes[m.tgtIdx]]
-
-	b.WriteString(headerStyle.Render("voca"))
-	b.WriteString("  ")
-	if m.focused == focusSrcLang {
-		b.WriteString(subtleStyle.Render("From:"))
-		b.WriteString(" ")
-		b.WriteString(inputStyle.Bold(true).Render(srcName))
-	} else {
-		b.WriteString(fmt.Sprintf("From: %s", srcName))
-	}
-	b.WriteString("  ->  ")
-	if m.focused == focusTgtLang {
-		b.WriteString(subtleStyle.Render("To:"))
-		b.WriteString(" ")
-		b.WriteString(outputStyle.Bold(true).Render(tgtName))
-	} else {
-		b.WriteString(fmt.Sprintf("To: %s", tgtName))
-	}
-	b.WriteString("\n\n")
-
-	b.WriteString(inputStyle.Render("Input"))
-	b.WriteString("\n")
-	b.WriteString(m.textarea.View())
-	b.WriteString("\n\n")
-
-	b.WriteString(outputStyle.Render("Output"))
-	b.WriteString("\n")
-	if m.output != "" {
-		b.WriteString(m.output)
-		b.WriteString("\n")
-	} else {
-		b.WriteString(subtleStyle.Render("Translation will appear here..."))
-		b.WriteString("\n")
-	}
-
-	b.WriteString(strings.Repeat("─", max(m.width-2, 0)))
-	b.WriteString("\n")
-	b.WriteString(m.status)
-	b.WriteString("  ")
-	b.WriteString(helpStyle.Render("ctrl+y:copy  ctrl+l:clear  ctrl+t:swap  ctrl+c:quit  tab:next"))
-
-	return b.String()
 }
