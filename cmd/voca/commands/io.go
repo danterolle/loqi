97d8ec337470b5c1998b3835ef2fcae85880325e
Dario Camonita <danterolle@parrotsec.org>
2026-06-19 17:05:24 +0200

    fix: treat non-file positional args as literal text in translate

--- cmd/voca/commands/io.go
+++ cmd/voca/commands/io.go
@@ -10,10 +10,13 @@ import (
 func ReadInput(args []string) (string, error) {
 	if len(args) > 0 {
 		data, err := ReadStdinOrFile(args)
-		if err != nil {
-			return "", err
+		if err == nil {
+			return strings.TrimSpace(string(data)), nil
 		}
-		return strings.TrimSpace(string(data)), nil
+		if os.IsNotExist(err) && isTerminal() {
+			return strings.TrimSpace(args[0]), nil
+		}
+		return "", err
 	}
 
 	data, err := readStdin()
@@ -21,6 +24,14 @@ func ReadInput(args []string) (string, error) {
 		return "", nil
 	}
 	return strings.TrimSpace(string(data)), nil
+}
+
+func isTerminal() bool {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+	return (stat.Mode() & os.ModeCharDevice) != 0
 }
 
 func ReadStdinOrFile(args []string) ([]byte, error) {
